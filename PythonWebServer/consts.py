a135aa30c5d5f38c6b36ce7dce71fd9ecfb5b64b
Shervin <68682979+shervin-glitch@users.noreply.github.com>
2021-12-08 16:12:53 -0800

    Update consts.py

--- PythonWebServer/consts.py
+++ PythonWebServer/consts.py
@@ -13,7 +13,7 @@ CONST_COLORS : List[Dict[int , str]] = [
 ]
 
 HOST : str = ""
-PORT : str = 8080
+PORT : int = 8080
 CONTENT_TYPE : str = "Content-type: text/html\n"
 HTTP_TYPE : str = "HTTP/1.0 200 OK\n"
 ENCODE : str = "iso-8859-1"
